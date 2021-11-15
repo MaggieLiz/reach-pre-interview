07a3bd18211e87d297afc537c24e6549ac6d4b5d
maggieliz <maggielizward@gmail.com>
2021-11-15 12:13:34 +0000

    Q1 and Q2 done. Need to complete Q3.

--- src/01-count-major-versions-above-10/index.js
+++ src/01-count-major-versions-above-10/index.js
@@ -31,6 +31,24 @@ The results should have this structure:
 
 module.exports = async function countMajorVersionsAbove10() {
   // TODO
+  const axios = require('axios')
+  const payload = {
+    'url': 'https://api.npms.io/v2/search/suggestions?q=react',
+    'method': 'GET',
+    'return_payload': true
+  }
 
+  const res = await axios.post('http://ambush-api.inyourarea.co.uk/ambush/intercept', payload)
+  const data =res.data
+
+  const versionsArray = data.content.map(unit => {
+    return unit.package.version
+  })
+
+  const overTenArray = versionsArray.filter(version => {
+    return parseFloat(version) >= 10
+  })
+
+  const count = overTenArray.length
   return count
 };
--- src/02-oldest-package-name/index.js
+++ src/02-oldest-package-name/index.js
@@ -30,6 +30,42 @@ The results should have this structure:
 
 module.exports = async function oldestPackageName() {
   // TODO
+  const axios = require('axios')
 
-  return name
+  const payload = {
+    'url': 'https://api.npms.io/v2/search/suggestions?q=react',
+    'method': 'GET',
+    'return_payload': true
+  }
+
+  const res = await axios.post('http://ambush-api.inyourarea.co.uk/ambush/intercept', payload)
+  const data =res.data
+
+  const packagesArray = data.content.map(pack => {
+    return pack.package
+    })
+
+  const datesArray = packagesArray.map(item => {
+    return item.date
+  })  
+
+  const compare = (a, b) => {
+    if (a < b) {
+      return -1
+    } else if (a > b) {
+      return 1
+    } return 0
+  }
+
+  const sortedArray = datesArray.sort(compare)
+
+  const oldestDate = sortedArray[0]
+  
+  const name = packagesArray.filter(item => {
+    if (item.date === oldestDate) {
+      return item
+    } 
+  })
+
+  return name[0].name
 };
